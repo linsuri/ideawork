b59cb41244fe8e977ab7a54f44d675bff1bfddd1
github username <github email address>
2018-11-22 16:25:17 -0500

    added reorder function in ContentContainer to use with css column-count property to get the masonry grid

--- src/ContentContainer.js
+++ src/ContentContainer.js
@@ -1,62 +1,31 @@
 import React from 'react'
 import Content from './Content'
 import ContentInfo from './ContentInfo'
-import { Grid, Segment } from 'semantic-ui-react'
 
 const ContentContainer = () => {
 
-  const ContentArr = ContentInfo.map(content => <Content key={content.id} imgsrc={content.imgsrc} date={content.date} title={content.title} url={content.url} />)
+  const reorder = (arr, columns) => {
+    const newContentArr = []
+    let currentColumn = 0
+
+    while (currentColumn < columns) {
+      for (let i = 0; i < arr.length; i += columns) {
+        if (arr[i + currentColumn]) {
+          newContentArr.push(arr[i + currentColumn])
+        }
+      }
+      currentColumn++
+    }
+
+    return newContentArr
+  }
 
   return (
     <div className="content-container">
-      {ContentArr}
-      {/* <Grid>
-        <Grid.Row columns={2} only='mobile tablet'>
-          <Grid.Column>
-            <Segment>1</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>2</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>3</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>4</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>5</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>6</Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-
-      <Grid>
-        <Grid.Row columns={3} only='computer'>
-          <Grid.Column>
-            <Segment>1</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>2</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>3</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>4</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>5</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>6</Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid> */}
+      {reorder(ContentInfo, 3).map(content => <Content key={content.id} imgsrc={content.imgsrc} date={content.date} title={content.title} url={content.url} />)}
     </div>
   )
+
 }
 
 export default ContentContainer

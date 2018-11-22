b9e4bd6579aa195d9369dbf9621c9dd6f2679e76
github username <github email address>
2018-11-22 11:48:02 -0500

    moved around asseets & created <Header/> & styled some css

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img className="logo" src="/logo.svg" alt="logo"/>
+      <p>212.555.5555</p>
+      <p>LOGIN</p>
+    </div>
+  )
+}
+
+export default Header

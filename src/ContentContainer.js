42c516c94affdf2006a253f0228bab5b3d6fd7ad
github username <github email address>
2018-11-22 12:58:36 -0500

    styled Footer's a tag

--- src/ContentContainer.js
+++ src/ContentContainer.js
@@ -1,10 +1,10 @@
 import React from 'react'
 // import Content from 'Content'
-// import ContentInfo from 'ContentInfo'
+import ContentInfo from 'ContentInfo'
 
 const ContentContainer = () => {
 
-  // const ContentArr = ContentInfo.map(content => <Content key={content.id} date={content.date} title={content.title} url={content.url} }/>)
+  // const ContentArr = ContentInfo.map(content => <Content key={content.id} imgsrc={content.imgsrc} date={content.date} title={content.title} url={content.url} }/>)
 
   return (
 

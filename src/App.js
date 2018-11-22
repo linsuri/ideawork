8adf721d7d155b1d7caf6eb96c7ddb38120f3ffe
github username <github email address>
2018-11-22 12:43:20 -0500

    added <ContentContainer />

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="app">
         <Header />
         {/* <Carousel />
-        <Grid /> */}
+        <ContentContainer /> */}
         <Footer />
       </div>
     );
---
+++ src/ContentContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Content from 'Content'
+// import ContentInfo from 'ContentInfo'
+
+const ContentContainer = () => {
+
+  // const ContentArr = ContentInfo.map((content, index) => <Content key={index} date={content.date} title={content.title} url={content.url} }/>)
+
+  return (
+
+  )
+}
+
+export default ContentContainer

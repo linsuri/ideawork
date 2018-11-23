77248b6ddf5609adc2ef7dc69d82b739e7fc636f
github username <github email address>
2018-11-22 19:43:03 -0500

    fixed fonts in css, not done

--- src/Content.js
+++ src/Content.js
@@ -6,7 +6,7 @@ const Content = (props) => {
       <img src={props.imgsrc} alt={props.title}/>
       <p>{props.date}</p>
       <h1>{props.title}</h1>
-      <p>Presented by <a href="https://www.google.com">LOREM IPSUM</a></p>
+      <p className="present">Presented by <a href="https://www.google.com">LOREM IPSUM</a></p>
       <hr/>
     </div>
   )

f491b2f4929a105af53259e3938c85e373aabce5
github username <github email address>
2018-11-22 21:54:10 -0500

    fixed styling and fonts

--- src/Content.js
+++ src/Content.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Content = (props) => {
   return (
     <div className="content">
-      <img src={props.imgsrc} alt={props.title}/>
-      <p>{props.date}</p>
-      <h1>{props.title}</h1>
+      <a href={props.url}><img src={props.imgsrc} alt={props.title}/></a>
+      <p className="date">{props.date}</p>
+      <a href={props.url}><h1 className="title">{props.title}</h1></a>
       <p className="present">Presented by <a href="https://www.google.com">LOREM IPSUM</a></p>
       <hr/>
     </div>

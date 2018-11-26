5eaa0d1a25e0a7900a29f8cb41b269fbe55d2032
github username <github email address>
2018-11-26 01:14:34 -0500

    fixed carousel according to spec

--- src/Content.js
+++ src/Content.js
@@ -4,9 +4,9 @@ const Content = (props) => {
   return (
     <div className="content">
       <a href={props.url}><img src={props.imgsrc} alt={props.title}/></a>
-      <p className="date">{props.date}</p>
-      <a href={props.url}><h1 className="title">{props.title}</h1></a>
-      <p className="present">Presented by <a href="https://www.google.com">LOREM IPSUM</a></p>
+      <p className="content-date">{props.date}</p>
+      <a href={props.url}><h1 className="content-title">{props.title}</h1></a>
+      <p className="content-present">Presented by <a href="https://www.google.com">LOREM IPSUM</a></p>
       <hr/>
     </div>
   )
--- src/SwipableCarousel.js
+++ src/SwipableCarousel.js
@@ -6,6 +6,12 @@ class SwipableCarousel extends React.Component {
   render() {
     return (
       <div className="carousel">
+        <div className="carousal-text">
+          <p className="carousal-title">Screening:</p>
+          <p className="carousal-title">DVF Secret Agent Part 2</p>
+          <p className="carousal-date">October 15, 2017</p>
+          <p className="carousal-p">Join us for a private screening of DVF Secret Agent Part 2 with our special guest, director Peter Lindberg.</p>
+        </div>
         <Carousel
           showArrows={false}
           showStatus={false}
@@ -17,13 +23,13 @@ class SwipableCarousel extends React.Component {
           swipeable={true}
         >
           <div>
-            <img className="carousel-img" src="img/carousel.png" alt="carousel" />
+            <img className="carousel-img" src="img/hero-landing.png" alt="carousel" />
           </div>
           <div>
-            <img className="carousel-img" src="img/carousel.png" alt="carousel" />
+            <img className="carousel-img" src="img/layer-60.png" alt="carousel" />
           </div>
           <div>
-            <img className="carousel-img" src="img/carousel.png" alt="carousel" />
+            <img className="carousel-img" src="img/layer-61.png" alt="carousel" />
           </div>
         </Carousel>
       </div>

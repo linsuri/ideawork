8988e657c2074b9e0aadfd646a1b1512792274c7
github username <github email address>
2018-11-27 14:22:53 -0500

    the dates are now dynamically ordered

--- src/ContentContainer.js
+++ src/ContentContainer.js
@@ -29,6 +29,10 @@ class ContentContainer extends React.Component {
   }
 
   reorder = (arr, columns) => {
+    // sort by date
+    arr = arr.sort((a, b) => new Date(b.date) - new Date(a.date))
+
+    // reorder to get the masonry grid
     const newContentArr = []
     let currentColumn = 0
 
@@ -45,7 +49,10 @@ class ContentContainer extends React.Component {
   }
 
   render() {
+    // if the screen is small, masonry grid has 2 columns
+    // if the screen is large, masonry grid has 3 columns
     let columns = (this.state.width > 812) || (768 < this.state.width && this.state.width < 1024 && this.state.width < this.state.height) ? 3 : 2
+
     return (
       <div className="content-container">
         {this.reorder(ContentInfo, columns).map(content => <Content key={content.id} imgsrc={content.imgsrc} date={content.date} title={content.title} url={content.url} />)}
